3cced7fb22dc15ed2b0d7ba3bdfacd4bd47c7533
Pablo Klijnjan <pablokl@il.ibm.com>
2013-11-19 17:11:32 +0200

    Adding technologies tab

--- app/app.js
+++ app/app.js
@@ -64,14 +64,40 @@ App.Skill.FIXTURES = [
 ];
 
 App.Technology.FIXTURES = [
-    { id: 1, name: 'vim', icon: '' },
-    { id: 2, name: 'tmux', icon: '' },
+    { id: 1, name: 'vagrant', icon: 'libs/img/vagrant.png' },
+    { id: 2, name: 'vim', icon: 'libs/img/vim.png' },
+    { id: 3, name: 'python', icon: 'libs/img/python-logo-generic.svg' },
+    { id: 4, name: 'docker', icon: 'libs/img/docker.png' },
+    { id: 5, name: 'emberjs', icon: 'libs/img/emberjs.png' },
+    { id: 6, name: 'js', icon: 'libs/img/js.png' },
+    { id: 7, name: 'jquery', icon: 'libs/img/jquery.png' },
+    { id: 8, name: 'django', icon: 'libs/img/django.png' },
+    { id: 9, name: 'HTML5', icon: 'libs/img/html5.png' },
+    { id: 10, name: 'vmware', icon: 'libs/img/vmware.jpg' },
+    { id: 11, name: 'postgresql', icon: 'libs/img/postgresql.png' },
+    { id: 12, name: 'git', icon: 'libs/img/git.png' },
+    { id: 13, name: 'svn', icon: 'libs/img/svn.png' },
+    { id: 14, name: 'raspberrypi', icon: 'libs/img/raspberrypi.svg' },
+    { id: 15, name: 'travis', icon: 'libs/img/travis.png' },
+    { id: 16, name: 'jenkins', icon: 'libs/img/jenkins.png' },
+    { id: 17, name: 'linux', icon: 'libs/img/linux.jpg' },
+    { id: 18, name: 'apache', icon: 'libs/img/apache.png' },
+    { id: 19, name: 'celery', icon: 'libs/img/celery.png' },
+    { id: 20, name: 'flask', icon: 'libs/img/flask.png' },
 ];
 
 App.Attribute.FIXTURES = [
     { id: 1, name: 'Python tests', rating: 5, advanced: false },
     { id: 2, name: 'Javascript tests', rating: 1, advanced: false },
 ];
+
+function getIds(model){
+    var ids = [];
+    $.each( model.FIXTURES, function(index, value){
+        ids.push(value.id);
+    });
+    return ids;
+}
 
 App.Character.FIXTURES = [
     {
@@ -82,8 +108,8 @@ App.Character.FIXTURES = [
         health: 100,
         mana: 80,
         geekiness: 90,
-        skills: [1, 2],
-        technologies: [1, 2],
-        attributes: [1, 2]
+        skills: getIds(App.Skill),
+        technologies: getIds(App.Technology),
+        attributes: getIds(App.Attribute)
     }
 ];
